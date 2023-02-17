02d206e40a230c827bdf9460e00f555133495806
Kornebari <your@email.here>
2023-02-17 17:35:08 -0600

    Starting CRUD for cards

--- api/models/cards.py
+++ api/models/cards.py
@@ -1,39 +1,48 @@
-# from pydantic import BaseModel
-# from typing import List, Optional
-# from bson.objectid import ObjectId
+from pydantic import BaseModel
+from typing import List, Optional
+from bson.objectid import ObjectId
 
 
-# class PydanticObjectId(ObjectId):
-#     @classmethod
-#     def __get_validators__(cls):
-#         yield cls.validate
+class PydanticObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-#     @classmethod
-#     def validate(cls, value: ObjectId | str) -> ObjectId:
-#         if value:
-#             try:
-#                 ObjectId(value)
-#             except ValueError:
-#                 raise ValueError(f"Not a valid object id: {value}")
-#         return value
+    @classmethod
+    def validate(cls, value: ObjectId | str) -> ObjectId:
+        if value:
+            try:
+                ObjectId(value)
+            except ValueError:
+                raise ValueError(f"Not a valid object id: {value}")
+        return value
 
 
-# class CardIn(BaseModel):
-#     name: str
-#     card_class: str
-#     hero_name: str
-#     hero_id: str
-#     series_name: str
-#     card_number: int
-#     effect_text: Optional[str]
-#     flavor_text: Optional[str]
-#     illustrator: Optional[str]
-#     picture_url: Optional[str]
-#     file_name: str
-#     card_type_id: int
-#     extra_effects: List
-#     reactions: List
-#     card_tags: List
+class CardIn(BaseModel):
+    name: str
+    card_class: str
+    hero_name: str
+    hero_id: str
+    series_name: str
+    card_number: int
+    effect_text: Optional[str]
+    flavor_text: Optional[str]
+    illustrator: Optional[str]
+    picture_url: Optional[str]
+    file_name: str
+    card_type_id: str
+    extra_effects: List
+    reactions: List
+    card_tags: List
 
-# class CardOut(CardIn):
-#     id: PydanticObjectId
+
+class Card(CardIn):
+    id: PydanticObjectId
+
+
+class CardOut(CardIn):
+    id: str
+
+
+class CardsAll(BaseModel):
+    cards: List[CardOut]
--- api/queries/cards.py
+++ api/queries/cards.py
@@ -1,24 +1,47 @@
-# from pydantic import BaseModel
-# from typing import Optional, List
-# from queries.pool import pool
-# # from queries.card_comps import CardTypeOut,
+from .client import Queries
+from bson.objectid import ObjectId
+from pymongo import ReturnDocument
+from models.cards import (
+    CardIn,
+    Card,
+    CardOut,
+    CardsAll
+)
 
-# class CardIn(BaseModel):
-#     name: str
-#     card_class: str
-#     hero_name: str
-#     hero_id: str
-#     series_name: str
-#     card_number: int
-#     effect_text: Optional[str]
-#     flavor_text: Optional[str]
-#     illustrator: Optional[str]
-#     picture_url: Optional[str]
-#     file_name: str
-#     card_type_id: int
-#     extra_effects: List
-#     reactions: List
-#     card_tags: List
 
-# class CardOut(CardIn):
-#     id: int
+class CardTypeQueries(Queries):
+    DB_NAME = "cards"
+    COLLECTION = "cards"
+
+    def get_all_cards(self) -> CardsAll:
+        db = self.collection.find()
+        cards = []
+        for document in db:
+            document["id"] = str(document["_id"])
+            cards.append(CardOut(**document))
+        return cards
+
+    def get_card(self, id) -> CardOut:
+        props = self.collection.find_one({"_id": ObjectId(id)})
+        if not props:
+            return None
+        props["id"] = str(props["_id"])
+        return CardOut(**props)
+
+    def create_card(self, card: CardIn) -> Card:
+        props = card.dict()
+        self.collection.insert_one(props)
+        props["id"] = str(props["_id"])
+        return Card(**props)
+
+    def update_card_type(self, id: str, card: CardIn) -> CardTypeOut:
+        props = card.dict()
+        self.collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": props},
+            return_document=ReturnDocument.AFTER,
+        )
+        return CardOut(**props, id=id)
+
+    def delete_card(self, id: str) -> bool:
+        return self.collection.delete_one({"_id": ObjectId(id)})
