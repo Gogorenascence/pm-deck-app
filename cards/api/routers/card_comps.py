dde2454889e9250457037d96f14205f8106552ef
Kornebari <your@email.here>
2023-02-17 09:07:50 -0600

    Removed response from card_type create router

--- cards/api/routers/card_comps.py
+++ cards/api/routers/card_comps.py
@@ -42,15 +42,11 @@ async def get_card_type(
 @router.post("/api/card_types/", response_model=CardTypeOut)
 async def create_card_type(
     card_type_in: CardTypeIn,
-    response: Response,
     queries: CardTypeQueries = Depends(),
     # account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     card_type = queries.create_card_type(card_type_in)
-    if card_type is None:
-        response.status_code = 404
-    else:
-        return card_type
+    return card_type
 
 @router.put("/api/card_types/{card_type_id}", response_model=CardTypeOut | str)
 async def update_card_type(
