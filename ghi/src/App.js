02e8f05626c362a7120de4a26ce41105ad30355b
Kornebari <your@email.here>
2023-02-25 18:39:05 -0600

    Start of frontend work

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import DecksPage from "./SiteComp/DecksPage"
+import CardsPage from "./SiteComp/CardsPage"
+import ArticlesPage from "./SiteComp/ArticlesPage"
+import GamePlayPage from "./SiteComp/GamePlayPage"
+import ForumPage from "./SiteComp/ForumPage"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div className="App">
+
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="/decks" element={<DecksPage />} />
+          <Route path="/cards" element={<CardsPage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/gameplay" element={<GamePlayPage />} />
+          <Route path="/forum" element={<ForumPage />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/Nav.js
@@ -0,0 +1,219 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-success">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          PlayMaker CardBase
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto">
+            <div className="dropdown mx-1">
+              <button
+                className="btn btn-success dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Decks
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/decks">
+                    Deck Builder
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/decks">
+                    Popular Decks
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/decks">
+                    Newest Decks
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/decks">
+                    Deck Search
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="dropdown mx-1">
+              <button
+                className="btn btn-success dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Cards
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/cards">
+                    Popular Cards
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/cards">
+                    Newest Cards
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/cards">
+                    New Series
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/cards">
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/cards">
+                    Card Search
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="dropdown mx-1">
+              <button
+                className="btn btn-success dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Articles
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/articles">
+                    Popular Articles
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/articles">
+                    Newest Articles
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/articles">
+                    Strategy Guides
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/articles">
+                    Series Lore
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/articles">
+                    Article Search
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="dropdown mx-1">
+              <button
+                className="btn btn-success dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Game Play
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/gameplay">
+                    How To Play
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/gameplay">
+                    Game Modes
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/gameplay">
+                    Formats
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/gameplay">
+                    Mechanics
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/gameplay">
+                    Restricted Lists
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown mx-1">
+              <button
+                className="btn btn-success dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Forum
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="/forum">
+                    Forum
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/forum">
+                    Users
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
