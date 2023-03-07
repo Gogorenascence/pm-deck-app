8977d2e872ec9c69399820033605773e881396f9
Kornebari <your@email.here>
2023-03-06 21:29:46 -0600

    Basic card list view complete

--- ghi/src/Cards/CardsPage.js
+++ ghi/src/Cards/CardsPage.js
@@ -1,3 +1,9 @@
+import {
+    Col,
+    Row,
+    Card,
+    Button,
+} from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 function CardsPage() {
@@ -8,7 +14,7 @@ function CardsPage() {
         const response = await fetch(`${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/cards/`);
         const data = await response.json();
 
-        setCards(data);
+        setCards(data.cards);
     };
 
     useEffect(() => {
@@ -16,9 +22,137 @@ function CardsPage() {
     })
 
     return (
-        <div>
+        <div className="white-space">
+            <h1 className="left-h1">Card Search</h1>
+            <h2 className="left">Search our collection of cards</h2>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Card Name Contains..."
+                style={{width: "740px", height: "37px"}}>
+            </input>
+            <br/>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Card Text Contains..."
+                style={{width: "740px", height: "37px"}}>
+            </input>
+            <br/>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Card Number"
+                style={{width: "177px", height: "37px"}}>
+            </input>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Hero ID"
+                style={{width: "177px", height: "37px"}}>
+            </input>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Series"
+                style={{width: "177px", height: "37px"}}>
+            </input>
+            <input
+                className="left"
+                type="text"
+                placeholder=" Illustrator"
+                style={{width: "177px", height: "37px"}}>
+            </input>
+            <br/>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Type"
+                style={{width: "115px", height: "37px"}}>
+                <option value="type">Type</option>
+                <option value="fighter">Fighter</option>
+                <option value="aura">Aura</option>
+                <option value="move">Move</option>
+                <option value="ending">Ending</option>
+                <option value="any_type">Any Type</option>
+                <option value="item">Item</option>
+                <option value="event">Event</option>
+                <option value="comeback">Comeback</option>
+            </select>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Class"
+                style={{width: "115px", height: "37px"}}>
+                <option value="class">Class</option>
+                <option value="staunch">Staunch</option>
+                <option value="power">Power</option>
+                <option value="unity">Unity</option>
+                <option value="canny">Canny</option>
+            </select>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Extra Effect"
+                style={{width: "115px", height: "37px"}}>
+                <option value="extra_effect">Extra Effect</option>
+                <option value="trigger">Trigger</option>
+                <option value="limited">Limited</option>
+                <option value="critical">Critical</option>
+            </select>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Reaction"
+                style={{width: "115px", height: "37px"}}>
+                <option value="reaction">Reaction</option>
+                <option value="block">Block</option>
+                <option value="counter">Counter</option>
+                <option value="endure">Endure</option>
+                <option value="redirect">Redirect</option>
+            </select>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Tag"
+                style={{width: "115px", height: "37px"}}>
+                <option value="tag">Tag</option>
+                <option value="5_hp">5 HP</option>
+                <option value="focus">Focus</option>
+                <option value="auto">Auto</option>
+                <option value="stay">Stay</option>
+                <option value="max_1">Max</option>
+            </select>
+            <select
+                className="left"
+                type="text"
+                placeholder=" Sorted By"
+                style={{width: "115px", height: "37px"}}>
+                <option value="sorted_by">Sorted By</option>
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
+                <option value="most_views">Popular</option>
+            </select>
+            <br/>
+            <Button className="left" variant="dark">Search Cards</Button>
+            <Button className="left" variant="dark">Reset Filters</Button>
+            <Button className="left" variant="dark">Random Card</Button>
+            <br/>
 
-        </div>
+            <Row xs={1} sm={2} md={3} lg={4} xl={4}>
+                {cards.map((card) => {
+                    return (
+                        <Col >
+                            <Card
+                                style={{ width: '370px', margin: '37px 5px 5px 5px'}}>
+                                <Card.Img src="i1g1pmf.png" alt="Card image" variant="bottom"/>
+                            </Card>
+                        </Col>
+                    );
+                })}
+            </Row>
+
+    </div>
     );
 }
 
