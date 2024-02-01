ddc359cd0084fa7c70be1abaf86fadf2c7685dc4
Kornebari <nantahkl@gmail.com>
2024-01-31 19:10:49 -0600

    Added stories CRUD and starting frontend

--- ghi/src/Articles/StoriesPage.js
+++ ghi/src/Articles/StoriesPage.js
@@ -1,70 +1,70 @@
-import { useState, useEffect, useContext } from "react";
-import { NavLink, useNavigate } from 'react-router-dom';
-import { AuthContext } from "../../context/AuthContext";
+// import { useState, useEffect, useContext } from "react";
+// import { NavLink, useNavigate } from 'react-router-dom';
+// import { AuthContext } from "../context/AuthContext";
 
 
-function TermsPage() {
+// function StoriesPage() {
 
-    const { account } = useContext(AuthContext)
+//     const { account } = useContext(AuthContext)
 
-    const [terms, setTerms ] = useState([]);
+//     const [stories, setStories] = useState([]);
 
-    const getTerms = async() =>{
-        const response = await fetch(`${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/terms/`);
-        const data = await response.json();
-        const sortedData = [...data].sort((a,b) => a.name.localeCompare(b.name));
-        setTerms(sortedData);
-    };
+//     const getStories = async() =>{
+//         const response = await fetch(`${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/stories/`);
+//         const data = await response.json();
+//         const sortedData = [...data].sort((a,b) => a.name.localeCompare(b.name));
+//         setStories(sortedData);
+//     };
 
-    useEffect(() => {
-        window.scroll(0, 0);
-        document.body.style.overflow = 'auto';
-        getTerms();
-        document.title = "Glossary and Rulings - PM CardBase"
-        return () => {
-            document.title = "PlayMaker CardBase"
-        };
-    // eslint-disable-next-line
-    },[]);
+//     useEffect(() => {
+//         window.scroll(0, 0);
+//         document.body.style.overflow = 'auto';
+//         getStories();
+//         document.title = "Glossary and Rulings - PM CardBase"
+//         return () => {
+//             document.title = "PlayMaker CardBase"
+//         };
+//     // eslint-disable-next-line
+//     },[]);
 
 
-    return (
-        <div className="white-space">
-            <div className="flex-items">
-                <h1 className="left-h1 margin-top-20">Glossary and Rulings</h1>
+//     return (
+//         <div className="white-space">
+//             <div className="flex-items">
+//                 <h1 className="left-h1 margin-top-20">Glossary and Rulings</h1>
 
-                { account && account.roles.includes("admin")?
-                    <NavLink to="/termcreate">
-                        <button
-                            className="left red margin-left-13">
-                            Create
-                        </button>
-                    </NavLink>:
-                null}
-            </div>
+//                 { account && account.roles.includes("admin")?
+//                     <NavLink to="/storycreate">
+//                         <button
+//                             className="left red margin-left-13">
+//                             Create
+//                         </button>
+//                     </NavLink>:
+//                 null}
+//             </div>
 
-            <div className="fullTableBorder">
-                {terms.map(function(term, index, arr) {
-                    return (
-                        <div className="flex" key={term.name}>
-                            <div className="table200">
-                                <h5 className="text-table aligned">{term.name}</h5>
-                            </div>
-                            <div className="tableText flex-items between-space">
-                                <h5 className="text-table-2">{term.text}</h5>
-                                { account && account.roles.includes("admin")?
-                                <NavLink className="nav-link" to={`/glossary/${term.id}`}>
-                                    <h5 className="text-table">[Edit]</h5>
-                                </NavLink>:
-                            null}
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+//             <div className="fullTableBorder">
+//                 {stories.map(function(story, index, arr) {
+//                     return (
+//                         <div className="flex" key={story.name}>
+//                             <div className="table200">
+//                                 <h5 className="text-table aligned">{story.name}</h5>
+//                             </div>
+//                             <div className="tableText flex-items between-space">
+//                                 <h5 className="text-table-2">{story.text}</h5>
+//                                 { account && account.roles.includes("admin")?
+//                                 <NavLink className="nav-link" to={`/news/${story.id}`}>
+//                                     <h5 className="text-table">[Edit]</h5>
+//                                 </NavLink>:
+//                             null}
+//                             </div>
+//                         </div>
+//                     );
+//                 })}
+//             </div>
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default TermsPage;
+// export default StoriesPage;
--- ghi/src/Articles/StoryCreatePage.js
+++ ghi/src/Articles/StoryCreatePage.js
@@ -1,159 +1,159 @@
-import { useState, useEffect, useContext } from "react";
-import { NavLink, useNavigate } from 'react-router-dom';
-import { AuthContext } from "../../context/AuthContext.js";
+// import { useState, useEffect, useContext } from "react";
+// import { NavLink, useNavigate } from 'react-router-dom';
+// import { AuthContext } from "../context/AuthContext.js";
 
 
-function TermCreate() {
+// function StoryCreate() {
 
-    const [term, setTerm ] = useState({
-        name: "",
-        text: "",
-        term_number: 1000,
-    });
+//     const [story, setStory ] = useState({
+//         name: "",
+//         text: "",
+//         story_number: 1000,
+//     });
 
-    const { account } = useContext(AuthContext)
-    const [stayHere, setStayHere] = useState(false)
+//     const { account } = useContext(AuthContext)
+//     const [stayHere, setStayHere] = useState(false)
 
-    const [terms, setTerms ] = useState([]);
-    const [termNumber, setTermNumber] = useState(0)
+//     const [stories, setStories ] = useState([]);
+//     const [storyNumber, setStoryNumber] = useState(0)
 
-    const getTerms = async() =>{
-        const response = await fetch(`${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/terms/`);
-        const data = await response.json();
-        const sortedData = [...data].sort((a,b) => a.name.localeCompare(b.name));
-        setTerms(sortedData);
-        const term_numbers = data.map(term => term.term_number)
-        const max_term_number = Math.max(...term_numbers)
-        setTermNumber(max_term_number + 1)
-    };
+//     const getStories = async() =>{
+//         const response = await fetch(`${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/stories/`);
+//         const data = await response.json();
+//         const sortedData = [...data].sort((a,b) => a.name.localeCompare(b.name));
+//         setStories(sortedData);
+//         const story_numbers = data.map(story => story.story_number)
+//         const max_story_number = Math.max(...story_numbers)
+//         setStoryNumber(max_story_number + 1)
+//     };
 
-    useEffect(() => {
-        window.scroll(0, 0);
-        getTerms();
-        document.body.style.overflow = 'auto';
-        document.title = "Term Create - PM CardBase"
-        return () => {
-            document.title = "PlayMaker CardBase"
-        };
-    // eslint-disable-next-line
-    },[]);
+//     useEffect(() => {
+//         window.scroll(0, 0);
+//         getStories();
+//         document.body.style.overflow = 'auto';
+//         document.title = "Story Create - PM CardBase"
+//         return () => {
+//             document.title = "PlayMaker CardBase"
+//         };
+//     // eslint-disable-next-line
+//     },[]);
 
-    const handleChange = (event) => {
-        setTerm({ ...term, [event.target.name]: event.target.value });
-        console.log(termNumber)
-    };
+//     const handleChange = (event) => {
+//         setStory({ ...story, [event.target.name]: event.target.value });
+//         console.log(storyNumber)
+//     };
 
-    const handleCheck = (event) => {
-        setStayHere(!stayHere);
-    };
+//     const handleCheck = (event) => {
+//         setStayHere(!stayHere);
+//     };
 
-    const navigate = useNavigate()
+//     const navigate = useNavigate()
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = {...term};
-        data["term_number"] = termNumber
-        const termUrl = `${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/terms/`;
-        const fetchConfig = {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
+//     const handleSubmit = async (event) => {
+//         event.preventDefault();
+//         const data = {...story};
+//         data["story_number"] = storyNumber
+//         const storyUrl = `${process.env.REACT_APP_FASTAPI_SERVICE_API_HOST}/api/stories/`;
+//         const fetchConfig = {
+//             method: "POST",
+//             body: JSON.stringify(data),
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         };
 
-        const response = await fetch(termUrl, fetchConfig);
-        if (response.ok) {
-            setTerm({
-                name: "",
-                text: "",
-                term_number: 1000
-            });
-            getTerms()
-            if (!stayHere) {navigate(`/glossary`)}
-            console.log("Success")
-        } else {
-            alert("Error in creating term");
-        }
-    }
+//         const response = await fetch(storyUrl, fetchConfig);
+//         if (response.ok) {
+//             setStory({
+//                 name: "",
+//                 text: "",
+//                 story_number: 1000
+//             });
+//             getStories()
+//             if (!stayHere) {navigate(`/glossary`)}
+//             console.log("Success")
+//         } else {
+//             alert("Error in creating story");
+//         }
+//     }
 
-    if (!(account && account.roles.includes("admin"))) {
-        setTimeout(function() {
-            window.location.href = `${process.env.PUBLIC_URL}/`
-        }, 3000);
-    }
+//     if (!(account && account.roles.includes("admin"))) {
+//         setTimeout(function() {
+//             window.location.href = `${process.env.PUBLIC_URL}/`
+//         }, 3000);
+//     }
 
 
-    return (
-        <div>
-            { account && account.roles.includes("admin")?
-                <div className="white-space">
-                    <h1 className="margin-top-40">Term Create</h1>
-                        <div style={{display: "flex", justifyContent: "center"}}>
-                            <div style={{width: "50%", display: "flex", justifyContent: "center"}}>
-                                <div
-                                    id="create-term-page">
-                                    <h2 className="left">Term Details</h2>
-                                    <h5 className="label">Name </h5>
-                                    <input
-                                        className="builder-input"
-                                        type="text"
-                                        placeholder=" Term Name"
-                                        onChange={handleChange}
-                                        name="name"
-                                        value={term.name}>
-                                    </input>
-                                    <br/>
-                                    <h5 className="label"> Text </h5>
-                                    <textarea
-                                        className="builder-text"
-                                        type="text"
-                                        placeholder=" Term Text"
-                                        onChange={handleChange}
-                                        name="text"
-                                        value={term.text}>
-                                    </textarea>
-                                    <br/>
+//     return (
+//         <div>
+//             { account && account.roles.includes("admin")?
+//                 <div className="white-space">
+//                     <h1 className="margin-top-40">Story Create</h1>
+//                         <div style={{display: "flex", justifyContent: "center"}}>
+//                             <div style={{width: "50%", display: "flex", justifyContent: "center"}}>
+//                                 <div
+//                                     id="create-story-page">
+//                                     <h2 className="left">Story Details</h2>
+//                                     <h5 className="label">Name </h5>
+//                                     <input
+//                                         className="builder-input"
+//                                         type="text"
+//                                         placeholder=" Story Name"
+//                                         onChange={handleChange}
+//                                         name="name"
+//                                         value={story.name}>
+//                                     </input>
+//                                     <br/>
+//                                     <h5 className="label"> Text </h5>
+//                                     <textarea
+//                                         className="builder-text"
+//                                         type="text"
+//                                         placeholder=" Story Text"
+//                                         onChange={handleChange}
+//                                         name="text"
+//                                         value={story.text}>
+//                                     </textarea>
+//                                     <br/>
 
-                                    <input
-                                        style={{margin: "20px 5px 9px 5px", height:"10px"}}
-                                        id="stayHere"
-                                        type="checkbox"
-                                        onChange={handleCheck}
-                                        name="stayHere"
-                                        checked={stayHere}>
-                                    </input>
-                                    <label for="stayHere"
-                                        className="bold"
-                                    >
-                                        Keep me here
-                                    </label>
+//                                     <input
+//                                         style={{margin: "20px 5px 9px 5px", height:"10px"}}
+//                                         id="stayHere"
+//                                         type="checkbox"
+//                                         onChange={handleCheck}
+//                                         name="stayHere"
+//                                         checked={stayHere}>
+//                                     </input>
+//                                     <label for="stayHere"
+//                                         className="bold"
+//                                     >
+//                                         Keep me here
+//                                     </label>
 
-                                    <br/>
-                                    {account?
-                                        <button
-                                            className="left"
-                                            onClick={handleSubmit}
-                                        >
-                                            Create Term
-                                        </button>:null
-                                    }
-                                    <br/>
-                                    { !account?
-                                        <h6 className="error">You must be logged in to create a term</h6>:
-                                    null
-                                    }
-                                </div>
-                            </div>
-                        </div>
-                </div>:
-                <div className="textwindow">
-                    <h1 className="undercontext">This Feature Is For Admins Only</h1>
-                    <h3 className="undercontext">Redirecting in 3 Seconds</h3>
-                </div>
-            }
-        </div>
-    );
-}
+//                                     <br/>
+//                                     {account?
+//                                         <button
+//                                             className="left"
+//                                             onClick={handleSubmit}
+//                                         >
+//                                             Create Story
+//                                         </button>:null
+//                                     }
+//                                     <br/>
+//                                     { !account?
+//                                         <h6 className="error">You must be logged in to create a story</h6>:
+//                                     null
+//                                     }
+//                                 </div>
+//                             </div>
+//                         </div>
+//                 </div>:
+//                 <div className="textwindow">
+//                     <h1 className="undercontext">This Feature Is For Admins Only</h1>
+//                     <h3 className="undercontext">Redirecting in 3 Seconds</h3>
+//                 </div>
+//             }
+//         </div>
+//     );
+// }
 
-export default TermCreate;
+// export default StoryCreate;
